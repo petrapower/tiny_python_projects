c17ab60afc4b7308032284f14ebff12f238bc563
Petra Power <petra.powered@gmail.com>
2021-02-06 22:09:59 -0800

    program takes input text argument and encodes any numbers using the jump five algorithm

---
+++ 04_jump_the_five/jump.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+"""
+Author : petra <petra@localhost>
+Date   : 2021-02-06
+Purpose: Rock the Casbah
+"""
+
+import argparse
+
+
+# --------------------------------------------------
+def get_args():
+    """Get command-line arguments"""
+
+    parser = argparse.ArgumentParser(
+        description='Jump the five',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('str',
+                        metavar='str',
+                        help='Input text')
+
+    return parser.parse_args()
+
+
+# --------------------------------------------------
+def main():
+    """Make a jazz noise here"""
+
+    args = get_args()
+    encoded_nums = {"1": "9",
+                    "2": "8",
+                    "3": "7",
+                    "4": "6",
+                    "5": "0",
+                    "6": "4",
+                    "7": "3",
+                    "8": "2",
+                    "9": "1",
+                    "0": "5"}
+
+    for char in args.str:
+        if char in encoded_nums.keys():
+            print(encoded_nums[char], end='')
+        else:
+            print(char, end='')
+
+
+# --------------------------------------------------
+if __name__ == '__main__':
+    main()
