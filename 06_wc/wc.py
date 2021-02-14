85d840d947b2496d798e08eb480d5ab24ff45047
Petra Power <petra.powered@gmail.com>
2021-02-13 21:06:29 -0800

    wordcount - recreating wc terminal program - given file(s), outputs counts of lines, words, bytes, and total if more than one file was given

---
+++ 06_wc/wc.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+"""
+Author : petra <petra@localhost>
+Date   : 2021-02-13
+Purpose: Rock the Casbah
+"""
+
+import argparse
+import sys
+
+
+# --------------------------------------------------
+def get_args():
+    """Get command-line arguments"""
+
+    parser = argparse.ArgumentParser(
+        description='Emulate wc (word count)',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('file',
+                        help='Input file(s)',
+                        metavar='FILE',
+                        type=argparse.FileType('rt'),   # type has to be readable file - already open for reading
+                        default=[sys.stdin],            # default is STDIN - no need to open(), it's accessible
+                        nargs='*')                      # zero or more args
+
+    return parser.parse_args()
+
+
+# --------------------------------------------------
+def main():
+    """Make a jazz noise here"""
+
+    args = get_args()
+
+    line_total, word_total, byte_total = 0, 0, 0
+
+    for fh in args.file:
+        line_count, word_count, byte_count = 0, 0, 0
+        for line in fh:
+            line_count += 1
+            word_count += len(line.split())
+            byte_count += len(line)
+        print(f'{line_count:8}{word_count:8}{byte_count:8} {fh.name}')
+        line_total += line_count
+        word_total += word_count
+        byte_total += byte_count
+        fh.close()
+
+    if len(args.file) > 1:
+        print(f'{line_total:8}{word_total:8}{byte_total:8} total')
+
+
+# --------------------------------------------------
+if __name__ == '__main__':
+    main()
