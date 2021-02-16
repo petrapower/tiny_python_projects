37e7153e948a109b5d724762f0a3b67efb227eb1
Petra Power <petra.powered@gmail.com>
2021-02-15 19:02:23 -0800

    master - stupidly started new chapter on master - created initial file, updated args to be accepted

---
+++ 07_gashlycrumb/gashlycrumb.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+"""
+Author : petra <petra@localhost>
+Date   : 2021-02-15
+Purpose: Rock the Casbah
+"""
+
+import argparse
+
+
+# --------------------------------------------------
+def get_args():
+    """Get command-line arguments"""
+
+    parser = argparse.ArgumentParser(
+        description='Rock the Casbah',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('letter',
+                        help='Letter(s)',
+                        metavar='letter',
+                        type=str,
+                        default='')
+
+    parser.add_argument('-f',
+                        '--file',
+                        help='Input file',
+                        metavar='FILE',
+                        type=argparse.FileType('rt'),
+                        default='gashlycrumb.txt')
+
+    return parser.parse_args()
+
+
+# --------------------------------------------------
+def main():
+    """Make a jazz noise here"""
+
+    args = get_args()
+    str_arg = args.arg
+    int_arg = args.int
+    file_arg = args.file
+    flag_arg = args.on
+    pos_arg = args.positional
+
+    print(f'str_arg = "{str_arg}"')
+    print(f'int_arg = "{int_arg}"')
+    print('file_arg = "{}"'.format(file_arg.name if file_arg else ''))
+    print(f'flag_arg = "{flag_arg}"')
+    print(f'positional = "{pos_arg}"')
+
+
+# --------------------------------------------------
+if __name__ == '__main__':
+    main()
